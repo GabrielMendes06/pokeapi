1403093f8d1c874b72115ff8d846c73109e3dce5
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-04-19 01:34:56 -0300

    push

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function loadpk() {
-    console.clear();
-    console.log("Estou na função")
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  let dados = url + nomePokemon.value;
+  console.log(dados);
+  fetch(dados)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.clear();
+      console.log(data);
+      document.querySelector("#nome").innerHTML = data["name"];
+      document.querySelector("#numero").innerHTML = data["id"];
+      let img = data["sprites"]["front_default"];
+      document.querySelector("#foto").setAttribute("src", img);
+    })
+    .catch((erro) => {
+      console.log(`Erro: ${erro}`);
+    });
 }
-
-document.querySelector("#but1").onclick = loadpk
-
+const nomePokemon = document.querySelector("#digita-nome")
+nomePokemon.addEventListener("keyup",function(e) {
+    if(e.key === 'Enter') {       
+        loadpk();
+        nomePokemon.value = ''
+    }
+  }) 
+document.querySelector("#but1").onclick = loadpk;
 
